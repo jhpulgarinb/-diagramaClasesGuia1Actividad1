ae5a328448bd2d8ea12c0cb25576da41e066c04b
jhpulgarin <158075859+jhpulgarinb@users.noreply.github.com>
2024-02-20 18:36:27 -0500

    Actividad 1 Guia 1 Diagramas de Clases
    Actividad 1 Guia 1 Diagramas de Clases

---
+++ Fecha.java
@@ -0,0 +1,21 @@
+public class Fecha {
+
+	private int año;
+
+	private int mes;
+
+	private int dia;
+
+	public boolean validarFecha() {
+		return false;
+	}
+
+	public char obtenerFechaActual() {
+		return 0;
+	}
+
+	public int calcularDiferenciaDias(Fecha fecha1, Fecha fecha2) {
+		return 0;
+	}
+
+}
